00c971b2f41d375b43c9cb6bb5aef53380aaf595
musawa <musawa@kali.kali>
2025-05-14 04:37:44 -0400

    commited

--- auth.ts
+++ auth.ts
@@ -6,8 +6,7 @@ import bcrypt from "bcrypt";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import { getUser } from "@/app/lib/action";
-import poolPromise from "@/app/lib/db";
-import sql from "mssql";
+import pool from "@/app/lib/db";
 
 const prisma = new PrismaClient();
 
@@ -33,24 +32,23 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
           const passwordsMatch = await bcrypt.compare(password, user.password);
           if (passwordsMatch) {
             try {
-              const pool = await poolPromise; // Ensure the pool is connected
+              // Ensure the pool is connected
               const sessionToken = `token-${Date.now()}`; // Generate a session token
               const expirationDate = new Date(Date.now() + 24 * 60 * 60 * 1000); // 24-hour expiration
 
               // Insert session into the database
-              const result = await pool
-                .request()
-                .input("userId", sql.VarChar, user.id) // Bind the user ID
-                .input("sessionToken", sql.VarChar, sessionToken) // Bind the session token
-                .input("expires", sql.DateTime, expirationDate) // Bind the expiration date
-                .query(`
-                  INSERT INTO [Session] (userId, sessionToken, expires)
-                  OUTPUT INSERTED.*
-                  VALUES (@userId, @sessionToken, @expires)
-                `);
+              const query = `
+                INSERT INTO "Session" ("userId", "sessionToken", "expires")
+                VALUES ($1, $2, $3)
+                RETURNING *
+              `;
 
-              const session = result.recordset[0]; // The newly created session
+              const values = [user.id, sessionToken, expirationDate];
 
+              const result = await pool.query(query, values);
+              const session = result.rows[0]; // The newly created session
+
+              console.log(session);
               // Return user with the role
               return { ...user, role: user.role };
             } catch (error) {
